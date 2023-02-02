3a9719392663db85bcd94091e359771505e4812c
guilhermesmatiola <guimatiola@gmail.com>
2023-02-02 16:40:19 -0300

    test get da nfc postada

--- src/routers/zplRouter.js
+++ src/routers/zplRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { teste, postFromNFC, postZPL, getZPLById, openZPLshorten , deleteURLid, getAllZPL} from "../controllers/zplController.js";
+import { teste, postFromNFC, postZPL, getZPLById, openZPLshorten , deleteURLid, getAllZPL, getFromNFC} from "../controllers/zplController.js";
 import { tokenValidationMiddleware } from "../middlewares/tokenSchemaValidation.js";
 import { ValidateZPL } from "../middlewares/zplSchemaValidation.js";
 
 const router = Router();
 
+router.get("/teste", getFromNFC)
 router.get("/teste", teste)
 router.post("/nfc", postFromNFC)
 router.post("/zpl", tokenValidationMiddleware, ValidateZPL, postZPL);
