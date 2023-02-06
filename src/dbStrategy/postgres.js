e608745e7f1209dfa52023f280229ffc64f1a6a8
guilhermesmatiola <guimatiola@gmail.com>
2023-02-06 14:11:32 -0300

    punch the clock with nfc / fix

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -6,15 +6,15 @@ const { Pool } = pg;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const databaseConfig = {
-      //connectionString: process.env.DATABASE_URL,
-      // ssl:{
-      //       rejectUnautorized: false
-      // },
-       host: 'localhost',
-       port: 5432,
-       user: 'postgres',
-       password: 'postgres',
-       database: 'zpl'
+      connectionString: process.env.DATABASE_URL,
+      ssl:{
+            rejectUnautorized: false
+      },
+    //    host: 'localhost',
+    //    port: 5432,
+    //    user: 'postgres',
+    //    password: 'postgres',
+    //    database: 'zpl'
    }
 
 const connection = new Pool(databaseConfig);
